2c05811a1e10983342207a9cedd04f4619bf36e3
Ashley Boswell <boswelas@oregonstate.edu>
2024-08-14 12:50:58 -0700

    styling refactor

--- app/pages/about/index.tsx
+++ app/pages/about/index.tsx
@@ -4,21 +4,22 @@ import ResumeButton from "@/components/resume-button";
 
 export default function Home() {
     return (
-        <div className="h-screen border-[#fff0d5] border-8 bg-[#161c33] text-white relative overflow-hidden">
+        <div className="max-h-[90%] md:h-screen p-10 border-[#fff0d5] border-8 bg-[#161c33] text-white relative overflow-hidden">
             <div>
                 <div className="absolute top-0 left-[28%] rotate-[24deg] w-full animate-pulse-once z-10">
                     <div className="border-b-[1.75rem] border-[#0d7d6d] w-full"></div>
                     <div className="border-b-[2rem] border-[#fff0d5] w-full mt-1"></div>
                     <div className="border-b-[1rem] border-[#ff8e4d] w-full mt-1"></div>
                 </div>
-                <div className="mt-[16%] ml-[8%]">
-                    <div className="flex flex-col max-w-[40%]">
+                <div className="mt-[15%] ml-[3%] lg:mt-[13%] lg:ml-[8%]">
+                    <div className="flex flex-col max-w-full lg:max-w-[40%]">
                         <h1 className="font-semibold text-7xl">Hi, I'm <span className="text-[#0d7d6d]">Ashley</span>.</h1>
                         <span className="mt-2 text-2xl">Thanks for dropping by!</span>
                         <span className="mt-6 text-xl">I'm a <span className="text-[#0d7d6d] font-semibold">fullstack developer</span> based in Los Angeles.</span>
-                        <span className="text-xl">I love building intuitive, user-friendly projects using <span className="text-[#0d7d6d] font-semibold">Python, Typescript, and React</span>.</span>
+                        <span className="text-xl">I love building intuitive, user-friendly applications.</span>
+                        <span className="text-xl">I especially like using <span className="text-[#0d7d6d] font-semibold">Python, Typescript, and React</span>.</span>
                     </div>
-                    <div className="flex items-center mt-5 space-x-4">
+                    <div className="flex items-center mt-6 space-x-4">
                         <div className="">
                             <ResumeButton />
                         </div>
@@ -30,7 +31,7 @@ export default function Home() {
                     <img
                         src="./computer.png"
                         alt="Computer"
-                        className="absolute top-0 right-0 h-[55%] object-cover mt-[12%] mr-[15%] hidden md:block hover:h-[60%] hover:mt-[10%] hover:mr-[13%]"
+                        className="absolute top-0 right-0 h-[55%] object-cover mt-[12%] mr-[15%] hidden lg:block hover:h-[60%] hover:mt-[10%] hover:mr-[13%]"
                     />
                 </div>
             </div>
--- app/pages/skills/index.tsx
+++ app/pages/skills/index.tsx
@@ -31,18 +31,18 @@ export default function Skills() {
     }, []);
 
     return (
-        <div className="h-screen bg-[#0d7d6d] border-[#161c33] border-8 relative overflow-hidden">
-            <div className="absolute mt-20 ml-4 left-4 w-[50%] h-24 z-0 dot opacity-80" />
+        <div className="max-h-[90%] lg:h-screen bg-[#0d7d6d] border-[#161c33] border-8 relative overflow-x-hidden">
+            <div className="absolute mt-[4%] h-20 lg:mt-20 ml-[2%] lg:ml-4 left-4 w-[95%] lg:w-[50%] lg:h-24 z-0 dot opacity-80" />
             <div className="relative z-10">
-                <h1 className="mt-6 lg:mt-24 ml-6 text-7xl text-white font-bold">My skills
+                <h1 className="mt-[6%] lg:mt-24 ml-[6%] text-7xl text-white font-bold">My skills
                     <span className='text-[#161c33]'>.</span>
                 </h1>
             </div>
 
-            <div className="grid grid-cols-2 mt-4 relative z-10">
+            <div className="lg:grid lg:grid-cols-2 mt-4 relative z-10">
                 <div className="skills-container">
                     {skillsData.map((category) => (
-                        <div key={category.category} className="mb-4">
+                        <div key={category.category} className="mb-3 md:mb-4">
                             <h2 className="text-4xl font-semibold ml-4 text-[#fff0d5]">{category.category}</h2>
                             <div className="skills-row ml-8 mt-2">
                                 {category.skills.map((skill) => (
@@ -58,22 +58,22 @@ export default function Skills() {
                     ))}
                 </div>
 
-                <div className="education w-[85%] h-[50%] mt-4 ml-12 relative">
-                    <h2 className="text-4xl ml-4 text-[#fff0d5] mb-5 font-semibold">Education</h2>
-                    <div className="absolute w-[94%] h-[80%] blue-lines-container rounded-xl opacity-80">
+                <div className="education w-[90%] ml-4 lg:w-[85%] lg:h-[50%] lg:mt-4 lg:ml-12 relative">
+                    <h2 className="text-4xl ml-4 text-[#fff0d5] mb-2 lg:mb-3 font-semibold">Education</h2>
+                    <div className="absolute w-[94%] h-[80%] blue-lines-container rounded-xl opacity-80 hidden md:block">
                         <div className="blue-lines w-full h-full rounded-lg"></div>
                     </div>
 
-                    <div className="box-container z-20 relative ml-5 mt-8">
-                        <div className="box">
-                            <div className="image">
+                    <div className="box-container z-20 pb-6 mt-2 relative md:ml-5 md:mt-8">
+                        <div className="box ">
+                            <div className="image hidden md:block">
                                 <img src='../osu.jpg' />
                             </div>
-                            <div className="content">
-                                <h3 className="degree">Bachelor of Science in Computer Science</h3>
-                                <h4 className="school">Oregon State University</h4>
-                                <p className="gpa">GPA: 3.75/4.0</p>
-                                <p>Coursework: Web Development, Software Engineering, Mobile App Development,
+                            <div className="content lg:ml-4 ml-3 lg:p-1 p-0.5">
+                                <h3 className="degree mt-1 lg:mt-2">Bachelor of Science in Computer Science</h3>
+                                <h4 className="school mt-1 lg:mt-2">Oregon State University</h4>
+                                <p className="gpa mt-1 lg:mt-2">GPA: 3.75/4.0</p>
+                                <p className='mt-1 lg:mt-2 mb-1 lg:mb-2'>Coursework: Web Development, Software Engineering, Mobile App Development,
                                     Databases,
                                     Data Structures, Algorithms, Operating Systems, Parallel Programming
                                 </p>
